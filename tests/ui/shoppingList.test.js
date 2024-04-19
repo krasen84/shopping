b3f3699d5a9bb6c376d7336f8472b70d2221a1e4
krasen84 <116521221+krasen84@users.noreply.github.com>
2024-04-19 03:31:31 +0200

    Fix UI TEST 2

--- tests/ui/shoppingList.test.js
+++ tests/ui/shoppingList.test.js
@@ -1,8 +1,8 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check shopping list page', async ({ page }) => {
-    await page.goto('https://shopping-8fpi.onrender.com/shopping-list');
-    const list = await page.$('dl');
+    await page.goto('https://shopping-8fpi.onrender.com/Shopping-List');
+    const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
   
