c6f68ec6de860b4f8d7de4e7f321c41ff49151e9
krasen84 <116521221+krasen84@users.noreply.github.com>
2024-04-19 03:22:31 +0200

    Add URL to UI Tests

--- tests/ui/shoppingList.test.js
+++ tests/ui/shoppingList.test.js
@@ -1,8 +1,8 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check shopping list page', async ({ page }) => {
-    await page.goto('http://localhost:8080/shopping-list');
-    const list = await page.$('ul');
+    await page.goto('https://shopping-8fpi.onrender.com/shopping-list');
+    const list = await page.$('dl');
     expect(list).toBeTruthy();
   });
   
